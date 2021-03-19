12e7ee96d6c8d0dd53c58eb56b1d9da1967269a0
Trey Guest <twguest@students.latrobe.edu.au>
2021-03-19 11:32:23 +1100

    Added functions script
    I'm just trying to help out Grant, man....

---
+++ functions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+functions
+
+@author: twguest
+"""
+
+
+def hello_world():
+    print("hello grant")
+    
+if __name__ == '__main__':
+    hello_world()
